44a5edaacff6730e2aa0fa7be30962da235147b3
Hrishikesh Zagade <hrishi.zagade@gmail.com>
2023-09-07 22:43:12 +0530

    9-7-2023

--- airtravel.py
+++ airtravel.py
@@ -89,22 +89,33 @@ class Flight:
 
 
 class Aircraft:
+    def num_seats(self):
+        rows, row_seats = self.seating_plan()
+        return len(rows) * len(row_seats)
 
-    def __init__(self, registration, model, num_rows, num_seats_per_row):
+    def __init__(self, registration):
         self._registration = registration
-        self._model = model
-        self._num_rows = num_rows
-        self._num_seats_per_row = num_seats_per_row
 
     def registration(self):
         return self._registration
 
+
+class AirbusA319(Aircraft):
+
     def model(self):
-        return self._model
+        return "Airbus A19"
 
     def seating_plan(self):
-        return (range(1, self._num_rows),
-                "ABCDEFGHJK"[:self._num_seats_per_row])
+        return range(1, 23), "ABCDEF"
+
+
+class Boeing777(Aircraft):
+
+    def model(self):
+        return "Boeing 777"
+
+    def seating_plan(self):
+        return range(1, 56), "ABCDEFGHJK"
 
 
 def console_card_printer(passenger, seat, flight_number, aircraft):
@@ -121,12 +132,19 @@ def console_card_printer(passenger, seat, flight_number, aircraft):
     print()
 
 
-def make_flight():
-    f = Flight("BA758", Aircraft("G-EUPT", "Airbus S319", 22, 6))
+def make_flights():
+    f = Flight("BA758", AirbusA319("G-EUPT"))
     f.allocate_seat("12A", "Hrishi")
     f.allocate_seat("12F", "Kiran")
     f.allocate_seat("12E", "Vivek")
     f.allocate_seat("2E", "Harvey")
     f.allocate_seat("1E", "Mike")
 
-    return f
+    g = Flight("AF72", Boeing777("G-GSPS"))
+    g.allocate_seat("12A", "Hrishi")
+    g.allocate_seat("12F", "Kiran")
+    g.allocate_seat("12E", "Vivek")
+    g.allocate_seat("2E", "Harvey")
+    g.allocate_seat("1E", "Mike")
+
+    return f, g
